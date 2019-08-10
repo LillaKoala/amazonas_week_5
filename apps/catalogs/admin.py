254b4f006c0c6e200dd2a82944e23d867970fc35
Cyntia Goulart <48091787+LillaKoala@users.noreply.github.com>
2019-08-10 20:45:11 -0300

    Semana 5
    Arquivos atualizados na semana 5 sem venv (backup)

---
+++ apps/catalogs/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from apps.catalogs.models import Product, Category, Comment
+
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Comment)
+
+
